53772d06f45bb9043484d15af214add828dc7c36
Mauricio Salazar <emsd@kth.se>
2021-12-09 01:40:43 +0100

    Save the results of the process

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ http: // docs.python.org / library / multiprocessing.html  # module-multiprocess
 import random
 import multiprocessing as mp
 from time import perf_counter
+import pickle
 
 def monte_carlo_pi_part(n):
 
@@ -20,24 +21,29 @@ def monte_carlo_pi_part(n):
         if x*x + y*y <= 1:
             count=count+1
 
-    #return
     return count
 
 
 if __name__=='__main__':
     random.seed()
+    process = 1
     np = mp.cpu_count()
     print('You have {0:1d} CPUs'.format(np))
 
     # Number of points to use for the Pi estimation
-    n = 10_000_000
+    n = 1_000_000
 
     # iterable with a list of points to generate in each worker
     # each worker process gets n/np number of points to calculate Pi from
 
     part_count = [n/np] * np
-
+    start = perf_counter()
     with mp.Pool(processes=np) as pool:
         count = pool.map(monte_carlo_pi_part, part_count)
 
+
     print("Esitmated value of Pi:: ", sum(count)/(n*1.0)*4)
+    print(f"Estimated time: {perf_counter() - start}")
+
+    with open(f"result_{process}.pkl", "wb") as file:
+        pickle.dump(count, file)
